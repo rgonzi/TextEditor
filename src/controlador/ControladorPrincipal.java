e51b51a5daf393199703a27cccd26f165824b788
Roger <rogergonzalezacosta@gmail.com>
2024-06-27 12:24:18 +0200

    afegir actionListeners al menuBar

--- src/controlador/ControladorPrincipal.java
+++ src/controlador/ControladorPrincipal.java
@@ -1,5 +1,9 @@
 package controlador;
 
+import java.awt.event.ActionEvent;
+
+import javax.swing.JMenuItem;
+
 import vista.MenuPrincipal;
 
 public class ControladorPrincipal implements ActionListener{
@@ -13,6 +17,42 @@ public class ControladorPrincipal implements ActionListener{
 		finestra.setVisible(true);
 		
 		//Afegim els Listeners necessaris
-		finestra.
+		for (JMenuItem item: finestra.getMenuItems()) {
+			item.addActionListener(this);
+		}
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		//En funció del nom del item dels menús realitzarem una acció
+		seleccionarOpcio(e.getActionCommand());
+	}
+	
+	private void seleccionarOpcio(String opcio) {
+		switch(opcio) {
+		case "Open":
+			//TODO Obrir un fitxer
+			break;
+		case "Save":
+			//TODO Guardar un fitxer sobreescrivint
+			break;
+		case "Save as...":
+			//TODO Guardar en un fitxer diferent
+			break;
+		case "Exit":
+			//TODO Tancar el programa preguntant si guardem canvis
+			break;
+		case "Copy":
+			//TODO Copiar + shortcut
+			break;
+		case "Paste":
+			//TODO Enganxar + shortcut
+			break;
+		case "Full Screen":
+			//TODO Fer gran la finestra
+			break;
+		case "About":
+			//TODO Implementar finestra About
+			break;
+		}
 	}
 }
