650482915723258eb38bbc2e0a478c940df4a33d
Roger <rogergonzalezacosta@gmail.com>
2024-06-27 12:07:30 +0200

    optimització del codi

--- src/vista/MenuPrincipal.java
+++ src/vista/MenuPrincipal.java
@@ -11,22 +11,23 @@ public class MenuPrincipal extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JMenuBar menuBar;
 	private JMenu fileMenu, editMenu, viewMenu, helpMenu;
-	private JMenuItem open, save, saveAs, exit, copy, paste, fullscreen, about;
+	private JMenuItem[] menuItems = new JMenuItem[8]; // open, save, saveAs, exit, copy, paste, fullscreen, about
 	private JScrollPane scrollPane;
 	private JTextArea textArea;
 
 	public MenuPrincipal() {
+		//Iniciem la finestra amb una mida predefinida i la centrem
 		setTitle("Text editor");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(800, 500);
 		setLocationRelativeTo(null);
 		
+		//Iniciem la resta d'elements
 		initialize();
-		
-		this.setVisible(true);
 	}
 	
 	private void initialize() {
+		//Creem un JScrollPane que contindrà un JTextArea
 		scrollPane = new JScrollPane();
 		scrollPane.setBorder(BorderFactory.createEmptyBorder(8, 8, 8, 8));
 		getContentPane().add(scrollPane, BorderLayout.CENTER);
@@ -35,36 +36,60 @@ public class MenuPrincipal extends JFrame {
 		textArea.setLineWrap(true);
 		scrollPane.setViewportView(textArea);
 		
+		//Creació del menuBar amb tots els menus
 		menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 		
 		fileMenu = new JMenu("File");
-		menuBar.add(fileMenu);
-		open = new JMenuItem("Open");
-		fileMenu.add(open);
-		save = new JMenuItem("Save");
-		fileMenu.add(save);
-		saveAs = new JMenuItem("Save as...");
-		fileMenu.add(saveAs);
-		exit = new JMenuItem("Exit");
-		fileMenu.add(exit);
-		
 		editMenu = new JMenu("Edit");
-		menuBar.add(editMenu);
-		copy = new JMenuItem("Copy");
-		editMenu.add(copy);
-		paste = new JMenuItem("Paste");
-		editMenu.add(paste);
-		
 		viewMenu = new JMenu("View");
-		menuBar.add(viewMenu);
-		fullscreen = new JMenuItem("Full Screen");
-		viewMenu.add(fullscreen);
-		
 		helpMenu = new JMenu("Help");
+		
+		menuBar.add(fileMenu);
+		menuBar.add(editMenu);
+		menuBar.add(viewMenu);
 		menuBar.add(helpMenu);
-		about = new JMenuItem("About");
-		helpMenu.add(about);
+		
+		//Creem els menuItems dels menus i els afegim als seus menus corresponents
+		menuItems[0] = new JMenuItem("Open");
+		menuItems[1] = new JMenuItem("Save");
+		menuItems[2] = new JMenuItem("Save as...");
+		menuItems[3] = new JMenuItem("Exit");
+		
+		menuItems[4] = new JMenuItem("Copy");
+		menuItems[5] = new JMenuItem("Paste");
+		
+		menuItems[6] = new JMenuItem("Full Screen");
+		
+		menuItems[7] = new JMenuItem("About");
+		
+		for(int i = 0; i < 4; i++) {
+			fileMenu.add(menuItems[i]);
+		}
+		
+		editMenu.add(menuItems[4]);
+		editMenu.add(menuItems[5]);
+		viewMenu.add(menuItems[6]);
+		helpMenu.add(menuItems[7]);
+		
+	}
+	
+	//Getters i setters
+
+	public JTextArea getTextArea() {
+		return textArea;
+	}
+
+	public JMenuItem[] getMenuItems() {
+		return menuItems;
+	}
+
+	public void setTextArea(JTextArea textArea) {
+		this.textArea = textArea;
+	}
+	
+	public void setMenuItems(JMenuItem[] menuItems) {
+		this.menuItems = menuItems;
 	}
 	
 }
