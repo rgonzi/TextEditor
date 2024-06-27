428103f8443560423c83c1e2bbd27dcf717cb0bc
Roger <rogergonzalezacosta@gmail.com>
2024-06-27 12:08:40 +0200

    creacio ControladorPrincipal

---
+++ src/controlador/ControladorPrincipal.java
@@ -0,0 +1,18 @@
+package controlador;
+
+import vista.MenuPrincipal;
+
+public class ControladorPrincipal implements ActionListener{
+
+	private MenuPrincipal finestra;
+	
+	
+	public ControladorPrincipal() {
+		//Construïm la finestra principal
+		finestra = new MenuPrincipal();
+		finestra.setVisible(true);
+		
+		//Afegim els Listeners necessaris
+		finestra.
+	}
+}
