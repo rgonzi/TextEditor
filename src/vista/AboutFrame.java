d62b18529c1f8a3774b2b09bce382c818e5a549a
Roger <rogergonzalezacosta@gmail.com>
2024-06-27 15:09:26 +0200

    implement about text

---
+++ src/vista/AboutFrame.java
@@ -0,0 +1,48 @@
+package vista;
+
+import java.awt.BorderLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+
+public class AboutFrame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel panel;
+	private JTextArea txtrAboutTexteditorversion;
+
+	public AboutFrame() {
+		setTitle("About");
+		setResizable(false);
+		setAlwaysOnTop(true);
+		setSize(350, 200);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		init();
+		
+	}
+	
+	private void init() {
+		//TODO Implementar finestra About
+		panel = new JPanel();
+		panel.setBorder(BorderFactory.createEmptyBorder(8, 8, 8, 8));
+		getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		txtrAboutTexteditorversion = new JTextArea();
+		txtrAboutTexteditorversion.setText("TextEditor ©"
+				+ "\nVersion: preRelease"
+				+ "\nRelease Date: -"
+				+ "\n\nDeveloped by: Roger González Acosta [rgonzi]"
+				+ "\nEmail: rogergonzalezacosta@gmail.com"
+				+ "\nGitHub: https://github.com/rgonzi");
+		txtrAboutTexteditorversion.setEditable(false);
+		panel.add(txtrAboutTexteditorversion);
+		
+	}
+
+}
